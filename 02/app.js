5075dfac08597c3b53ac6229d8661c90cbe776a4
caffeinejun <gurwns232e@gmail.com>
2022-03-10 17:43:19 +0900

    02. 변수할당, boolean, 객체데이터, Date()

--- 02/app.js
+++ 02/app.js
@@ -1,47 +1,57 @@
-/*
-2주차 실습
+// /*
+// 2주차 실습
 
-*/
-var name = 'hj hello'
+// */
+// var name = 'hj hello'
+// console.log(name)
+
+// var isMan1 = true
+// var isMan2 = 'true'
+
+// console.log(isMan1)
+// console.log(isMan2)
+
+// function log(str){
+//     console.log(str)
+// }
+
+// log('hello')
+
+// // 객체
+// var img = {}
+// img.width = 1
+// img.height = 2
+// console.log(img)
+
+// // 배열
+// let arr = [1,2]
+// arr.push(1)
+// arr.push(13)
+// console.log(arr)
+
+// // 연산자
+// console.log(1+1)
+// console.log(name + 10)
+
+
+// const date = new Date()
+// console.log(date)
+
+
+let age = 20
+console.log(age)
+let name = false
 console.log(name)
-
-var isMan1 = true
-var isMan2 = 'true'
-
-console.log(isMan1)
-console.log(isMan2)
-
-function log(str){
-    console.log(str)
-}
-
-log('hello')
-
-// 객체
-var img = {}
-img.width = 1
-img.height = 2
-console.log(img)
-
-// 배열
-let arr = [1,2]
-arr.push(1)
-arr.push(13)
-console.log(arr)
-
-// 연산자
-console.log(1+1)
-console.log(name + 10)
+const month = new Date()
+console.log(month)
 
 let me = {
     age: 11,
     name: 'hj',
     isMan: true,
     sayHello: function(){
-        console.log('method hello')
+        console.log('hello')
     }
 }
-
+console.log(me)
 me.sayHello()
-
-
