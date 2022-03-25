d7f9a15bf2a8e538d21f670883e907720d122911
caffeinejun <gurwns232e@gmail.com>
2022-03-25 21:07:20 +0900

    quiz2 수정

---
+++ 04/quiz2/quiz2.js
@@ -0,0 +1,20 @@
+var board = document.getElementById('board')
+for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+        const span = document.createElement('span')
+        if (i % 2 === 0) {
+            j % 2 === 0 ? span.classList.add('black') : span.classList.add('white')
+        } else {
+            j % 2 === 0 ? span.classList.add('white') : span.classList.add('black')
+        }
+        board.appendChild(span)
+    }
+}
+
+board.addEventListener('click', e => {
+    const exist = document.querySelector('.clicked')
+    if (exist) {
+        exist.classList.remove('clicked')
+    }
+    e.target.classList.add('clicked')
+})
