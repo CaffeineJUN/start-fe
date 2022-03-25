4cb4ebdc8f04da33c2cab5ee66a7ad6469596e14
caffeinejun <gurwns232e@gmail.com>
2022-03-25 22:45:40 +0900

    quiz2 클릭event 예외 수정

--- 04/quiz2/quiz2.js
+++ 04/quiz2/quiz2.js
@@ -2,20 +2,19 @@ var board = document.getElementById('board')
 for (var i = 0; i < 4; i++) {
     for (var j = 0; j < 4; j++) {
         const span = document.createElement('span')
-        if (i % 2 === 0) {
-            j % 2 === 0 ? span.classList.add('black') : span.classList.add('white')
-        } else {
-            j % 2 === 0 ? span.classList.add('white') : span.classList.add('black')
-        }
+
+        if (i % 2 === 0) j % 2 === 0 ? span.classList.add('black') : span.classList.add('white')
+        else j % 2 === 0 ? span.classList.add('white') : span.classList.add('black')
+
         board.appendChild(span)
     }
 }
 
 board.addEventListener('click', e => {
-    const exist = document.querySelector('.clicked')
-    if (exist) {
-        exist.classList.remove('clicked')
-    }
+    if (e.target === e.currentTarget) return null
 
+    const exist = document.querySelector('.clicked')
+
+    if (exist) exist.classList.remove('clicked')
     e.target.classList.add('clicked')
 })
