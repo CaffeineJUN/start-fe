bd8d9c87df775a6870e9a0b346d3a33f32cfab15
caffeinejun <gurwns232e@gmail.com>
2022-04-07 20:12:05 +0900

    quiz7 풀이

---
+++ 06/quiz7/quiz7.js
@@ -0,0 +1,28 @@
+// 0~380 Math.random() * 381
+const box = document.querySelector('.box')
+const bug = document.querySelector('#bug')
+const point = document.querySelector('#point')
+const life = document.querySelector('#life')
+bug.style.visibility = 'hidden'
+
+function gameStart() {
+    bug.style.visibility = 'visible'
+
+    const interval = setInterval(() => {
+        bug.style.top = Math.random() * 381 + 'px'
+        bug.style.left = Math.random() * 381 + 'px'
+    }, 2000)
+
+    function score(e) {
+        e.target !== e.currentTarget ? point.innerHTML++ : life.innerHTML--
+        if (life.innerHTML === '0') {
+            alert('gameover')
+            point.innerHTML = 0
+            life.innerHTML = 10
+        }
+    }
+
+    box.addEventListener('click', score)
+}
+
+gameStart()
