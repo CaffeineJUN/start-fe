2e22186b8494c4ccc0c051924fe2e14b0ceaef5f
caffeinejun <gurwns232e@gmail.com>
2022-04-10 23:19:43 +0900

    quiz5 수정

--- 06/quiz5/quiz5.js
+++ 06/quiz5/quiz5.js
@@ -1,30 +1,34 @@
-const box = document.querySelectorAll('.box')
+const boxAll = document.querySelectorAll('.box')
+const box = document.querySelector('.box')
+const box1 = document.querySelector('.box.box1')
 
 function dragAndDrop(e) {
     const dragBox = e.currentTarget
 
-    dragBox.style.position = 'absolute'
-    document.body.append(dragBox)
-
-    let shiftX = e.clientX - dragBox.getBoundingClientRect().left
-    let shiftY = e.clientY - dragBox.getBoundingClientRect().top
+    let posX = e.clientX - dragBox.getBoundingClientRect().left
+    let posY = e.clientY - dragBox.getBoundingClientRect().top
 
     function boxMove(e) {
-        dragBox.style.left = e.pageX - shiftX + 'px'
-        dragBox.style.top = e.pageY - shiftY + 'px'
+        dragBox.style.left = e.pageX - posX + 'px'
+        dragBox.style.top = e.pageY - posY + 'px'
     }
 
     boxMove(e)
 
     document.addEventListener('mousemove', boxMove)
-
     dragBox.addEventListener('mouseup', () => {
         document.removeEventListener('mousemove', boxMove)
     })
 }
 
+function mousedown(element) {
+    element.addEventListener('mousedown', dragAndDrop)
+}
+
 function init() {
-    box.forEach(e => e.addEventListener('mousedown', dragAndDrop))
+    // boxAll.forEach(e => mousedown(e))
+    mousedown(box)
+    mousedown(box1)
 }
 
 init()
